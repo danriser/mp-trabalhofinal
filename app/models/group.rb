46afb48987cfe87b43d68fc5862db5aa8c6fdc26
pedro <pedrobsb0303@gmail.com>
2023-06-17 16:10:23 -0300

    tabela group criada

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     validates :type, presence: :true
-    validates :group_name, presence: :true
+    validates :group_name, presence: :true, uniqueness: :true
+    validates :description, presence: :true
+    validates :preference_id, presence: :true, uniqueness: :true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -11,9 +11,35 @@ RSpec.describe Group, type: :model do
     end
   end
 
-  context "validating group name" do
+  context "validating group name presence" do
     it "should be invalid if group_name=nil" do
       expect(build(:group,group_name:nil,type:"A")).to be_invalid
+    end
+  end
+
+  context "validating group name uniqueness" do
+    it "should be invalid if group_name already taken" do
+      create(:group,group_name:"A",type:"A")
+      expect(build(:group,group_name:"A",type:"A")).to be_invalid
+    end
+  end
+
+  context "validating group description" do
+    it "should be invalid if description=nil" do
+      expect(build(:group,description:nil,group_name:"B",type:"B")).to be_invalid
+    end
+  end
+
+  context "validating group preference" do
+    it "should be invalid if group preference=nil" do
+      expect(build(:group,group_name:"a",type:"a",description:"a",preference_id:nil)).to be_invalid
+    end
+  end
+
+  context "validating group preference uniqueness" do
+    it "should be invalid if group preference already taken" do
+      create(:group,group_name:"a",type:"a",description:"a",preference_id:2)
+      expect(build(:group,group_name:"b",type:"b",description:"b",preference_id:2)).to be_invalid
     end
   end
 
