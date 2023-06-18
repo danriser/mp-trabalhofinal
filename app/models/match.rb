7b851f5e2898ca1bed2960e4f9c8ea045f4d1651
pedro <pedrobsb0303@gmail.com>
2023-06-18 12:00:18 -0300

    testes para tabela match (funcionando)

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,6 @@
 class Match < ApplicationRecord
   belongs_to :user
+  validates :match_grade, presence: :true
+  validates :user_id, presence: :true
+  validates :user_id2, presence: :true
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "testing factory" do
+    it do
+      create(:user,id:1,nome:"joao",senha:"joao")
+      create(:user,id:2,nome:"aa",senha:"aa")
+      expect(build(:match,user_id:1,match_grade:0,user_id2:2)).to be_valid
+    end
+  end
+
+  context "validating users presence" do
+    it "should be invalid if user_id=nil or user_id2=nil" do
+      expect(build(:match,user_id:nil,user_id2:nil)).to be_invalid  
+    end
+  end
+
+  context "validating match_grade presence" do
+    it "should be invalid if match_grade=nil" do
+      create(:user,id:1,nome:"joao",senha:"joao")
+      create(:user,id:2,nome:"aa",senha:"aa")
+      expect(build(:match,user_id:1,match_grade:nil,user_id2:2)).to be_invalid
+    end
+  end
 end
