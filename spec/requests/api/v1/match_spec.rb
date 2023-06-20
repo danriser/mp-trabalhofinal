9b74d35b15e8a2ea4480cd55345c1fcc568a9abb
pedro <pedrobsb0303@gmail.com>
2023-06-20 20:32:58 -0300

    testes pra match_index

--- spec/requests/api/v1/match_spec.rb
+++ spec/requests/api/v1/match_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Api::V1::Matches", type: :request do
     before do
       create(:user,id:1,nome:"a",senha:"a")
       create(:user,id:2,nome:"b",senha:"b")
-      create(:match,user_id:1,user_id2:2,match_grade:3)
+      create(:match,id:1,user_id:1,user_id2:2,match_grade:"3")
     end
 
     context "index return" do
@@ -17,13 +17,28 @@ RSpec.describe "Api::V1::Matches", type: :request do
       end
 
       context "index return" do
+
         it "should return http status ok" do
           expect(response).to have_http_status(:ok)
+        end
+
+        it "should return json" do
+          expect(response.content_type).to eq("application/json; charset=utf-8")
+        end
+
+        it "should return created instances" do
+          expect(JSON.parse(response.body)).to eq([{
+            "created_at" => eval(Match.find(1).created_at.to_json),
+            "updated_at" => eval(Match.find(1).updated_at.to_json),
+            "id" => 1,
+            "user_id" => 1,
+            "user_id2" => 2,
+            "match_grade" => "3"
+          }])
         end
       end
 
     end
-
 
 
   end
