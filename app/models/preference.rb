3b067ea54fd7c41da314bb8804a7b8f13d8866f8
Humberto Alves Mesquita <humbertoalvesti@gmail.com>
2023-06-23 11:18:09 -0300

    teste documentacao

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,18 @@
+#
+# Model {Preference} 
+#
+# Responsável por armazenar os dados de uma preferencia
+#
+# @attribute id
+#   @return [int] id da preferencia
+#
+# @attribute tipo
+#   @return [String] tipo de preferencia
+#
+# @attribute descricao
+#   @return [String] descricao de uma preferencia
+#
+#
 class Preference < ApplicationRecord
     has_many :list_preferences
     has_one :groups
